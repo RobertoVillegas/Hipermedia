cd6b9b8cb02b82e37c02ba6769c7b88a1ef420e6
Lourdes <lrdslerma@gmail.com>
2021-05-24 12:22:45 -0500

    Escena 1 y 2 parcial

--- js/script.js
+++ js/script.js
@@ -45,14 +45,17 @@ monogatari.assets('music', {
 monogatari.assets('voices', {});
 
 // Define the sounds used in the game.
-monogatari.assets('sounds', {});
+monogatari.assets('sounds', {
+
+  'riverFlow': 'river.mp3',
+  'babyLaughing': 'babyLaughing.mp3'
+});
 
 // Define the videos used in the game.
 monogatari.assets('videos', {});
 
 // Define the images used in the game.
 monogatari.assets('images', {
-
 });
 
 // Define the backgrounds for each scene.
@@ -62,19 +65,13 @@ monogatari.assets('scenes', {
 
 // Define the Characters
 monogatari.characters({
-  y: {
-    name: 'Yui',
-    color: '#5bcaff',
-    sprites: {
-      normal: 'ProtagonistaGlobo.png',
-    },
-  },
+
   r: {
     name: 'Rey',
     color: '#FFE628',
-    directory: 'king',
+    directory: 'rey',
     sprites: {
-      normal: 'king1.jpg',
+      normal: 'rey.png',
     },
     expressions: {
       angry: 'expressions/normal.png',
@@ -91,8 +88,8 @@ monogatari.characters({
       normal: 'normal.jpg',
     },
     expressions: {
-      angry: 'expressions/normal.jpg',
-      normal: 'expressions/normal.jpg',
+      angry: 'expressions/normal.png',
+      normal: 'expressions/normal.png',
 
     },
     default_expression: 'normal'
@@ -100,12 +97,33 @@ monogatari.characters({
 
   p: {
     name: 'Padres',
-    color: '#5bcaff',
+    color: '#9FBD6E',
+    directory: 'padres',
     sprites: {
-      normal: 'padres.jpg',
+      normal: 'padres.png',
     },
+    expressions: {
+      angry: 'expressions/normal.png',
+      normal: 'expressions/normal.png',
+
+    },
+    default_expression: 'normal'
   },
 
+  m: {
+    name: 'Molineros',
+    color: '#9FBD6E',
+    directory: 'adoptivos',
+    sprites: {
+      normal: 'adoptivos.png',
+    },
+    expressions: {
+      angry: 'expressions/normal.png',
+      normal: 'expressions/normal.png',
+
+    },
+    default_expression: 'normal'
+  },
 
 
 });
@@ -119,42 +137,15 @@ monogatari.script({
   Escena1: [
     'play music mainTheme with loop with volume 50',
     'show scene #191A21 with fadeIn',
+    'show particles fireSparks',
 
-
-    // 'show notification Welcome',
-    // {
-    //   Input: {
-    //     Text: 'What is your name?',
-    //     Validation: function (input) {
-    //       return input.trim().length > 0;
-    //     },
-    //     Save: function (input) {
-    //       this.storage({
-    //         player: {
-    //           name: input,
-    //         },
-    //       });
-    //       return true;
-    //     },
-    //     Revert: function () {
-    //       this.storage({
-    //         player: {
-    //           name: '',
-    //         },
-    //       });
-    //     },
-    //     Warning: 'You must enter a name!',
-    //   },
-    // },
-    // 'show character y normal at center with fadeInUp',
-    // ' y Hi {{player.name}} Welcome to Monogatari!',
 
 
     'centered Érase una vez una mujer muy pobre que dio a luz un niño.',
     'centered Como el pequeño vino al mundo envuelto en la tela de la suerte, predijéronle que al cumplir los catorce años se casaría con la hija del Rey.',
     'centered Ocurrió que unos días después el Rey pasó por el pueblo,',
     'show background #343746 with fadeIn',
-    'show image pueblo.png center with fadeIn',
+    'show image 1padres.png center with fadeIn',
 
 
     'Sin darse a conocer, y al preguntar qué novedades había, le respondieron:',
@@ -164,61 +155,38 @@ monogatari.script({
 
     'El Rey, que era hombre de corazón duro, se irritó al oír aquella profecía,',
     // 'show background #000000 with fadeIn',
-    'show character p normal at right with fadeInUp',
+
     'Y, yendo a encontrar a los padres, les dijo con tono muy amable:',
-    'show character r normal at left with fadeInUp',
+
     'r:normal Vosotros sois muy pobres; dejadme, pues, a vuestro hijo, que yo lo cuidaré.',
     'Al principio, el matrimonio se negaba,',
     'pero al ofrecerles el forastero un buen bolso de oro, pensaron:',
-    'Ha nacido con buena estrella; será, pues, por su bien',
+    'p:normal Ha nacido con buena estrella; será, pues, por su bien',
     'y, al fin, aceptaron y le entregaron el niño.',
-    'hide character p with fadeOut',
-    'hide character r with fadeOut',
+
+    'play sound riverFlow with fade 15 with loop with volume 25',
+
     'El Rey lo metió en una cajita y prosiguió con él su camino, hasta que llegó al borde de un profundo río.',
     'Arrojó al agua la caja, y pensó:',
     'r:normal Así he librado a mi hija de un pretendiente bien inesperado.',
     'Pero la caja, en lugar de irse al fondo, se puso a flotar como un barquito, sin que entrara en ella ni una gota de agua.',
     'Y así continuó, corriente abajo, hasta cosa de dos millas de la capital del reino, donde quedó detenida en la presa de un molino.',
+
+    'hide image 1padres.png with fadeOut',
+    'show image 1bebecaja.png center with fadeIn',
+
     'Uno de los mozos, que por fortuna se encontraba presente y la vio, sacó la caja con un gancho, creyendo encontrar en ella algún tesoro.',
+
+
+    'play sound babyLaughing with volume 40',
     'Al abrirla ofreciéndose a su vista un hermoso chiquillo, alegre y vivaracho. Llevándolo el mozo al molinero Y su mujer, que, como no tenían hijos, exclamaron:',
-    '¡Es Dios que nos lo envía!',
+    'stop sound riverFlow with fade 12',
+    'm:normal ¡Es Dios que nos lo envía!',
+    'hide image 1bebecaja.png with fadeOut',
     'show background #191A21 with fadeIn',
     'centered Y cuidaron con todo cariño al niño abandonado, el cual creció en edad, salud y buenas cualidades.',
     'jump Escena2',
 
 
-
-
-    //   {
-    //     Choice: {
-    //       Dialog: 'y Have you already read some documentation?',
-    //       Yes: {
-    //         Text: 'Yes',
-    //         Do: 'jump Escena2',
-    //       },
-    //       No: {
-    //         Text: 'No',
-    //         Do: 'jump Escena2',
-    //       },
-    //     },
-    //   },
-    // ],
-
-    // Yes: [
-    //   'y Thats awesome!',
-    //   'y Then you are ready to go ahead and create an amazing Game!',
-    //   'y I can’t wait to see what story you’ll tell!',
-    //   'jump Escena2',
-    // ],
-
-    // No: [
-    //   'y You can do it now.',
-
-    //   'show message Help',
-
-    //   'y Go ahead and create an amazing Game!',
-    //   'y I can’t wait to see what story you’ll tell!',
-    //   'jump Escena2',
-    // ],
   ]
 });
--- js/script2.js
+++ js/script2.js
@@ -22,7 +22,119 @@ monogatari.action('notification').notifications({
 });
 
 // Define the Particles JS Configurations used in the game
-monogatari.action('particles').particles({});
+monogatari.action('particles').particles({
+
+  'rain': {
+    'particles': {
+      'number': {
+        'value': 400,
+        'density': {
+          'enable': true,
+          'value_area': 3000
+        }
+      },
+      'color': {
+        'value': '#e0e0e0'
+      },
+      'shape': {
+        'type': 'line',
+        'stroke': {
+          'width': 3,
+          'color': '#ffffff'
+        },
+        'polygon': {
+          'nb_sides': 2
+        },
+        'image': {
+          'src': 'img/github.svg',
+          'width': 100,
+          'height': 100
+        }
+      },
+      'opacity': {
+        'value': 0.8,
+        'random': true,
+        'anim': {
+          'enable': false,
+          'speed': 1,
+          'opacity_min': 0.1,
+          'sync': false
+        }
+      },
+      'size': {
+        'value': 2,
+        'random': true,
+        'anim': {
+          'enable': false,
+          'speed': 5,
+          'size_min': 0,
+          'sync': false
+        }
+      },
+      'line_linked': {
+        'enable': false,
+        'distance': 500,
+        'color': '#ffffff',
+        'opacity': 0.8,
+        'width': 2
+      },
+      'move': {
+        'enable': true,
+        'speed': 20,
+        'direction': 'bottom',
+        'random': false,
+        'straight': true,
+        'out_mode': 'out',
+        'bounce': false,
+        'attract': {
+          'enable': false,
+          'rotateX': 0,
+          'rotateY': 1200
+        }
+      }
+    },
+    'interactivity': {
+      'detect_on': 'canvas',
+      'events': {
+        'onhover': {
+          'enable': false,
+          'mode': 'bubble'
+        },
+        'onclick': {
+          'enable': false,
+          'mode': 'repulse'
+        },
+        'resize': true
+      },
+      'modes': {
+        'grab': {
+          'distance': 400,
+          'line_linked': {
+            'opacity': 0.5
+          }
+        },
+        'bubble': {
+          'distance': 400,
+          'size': 4,
+          'duration': 0.3,
+          'opacity': 1,
+          'speed': 3
+        },
+        'repulse': {
+          'distance': 200,
+          'duration': 0.4
+        },
+        'push': {
+          'particles_nb': 4
+        },
+        'remove': {
+          'particles_nb': 2
+        }
+      }
+    },
+    'retina_detect': true
+  }
+});
 
 // Define the canvas objects used in the game
 monogatari.action('canvas').objects({});
@@ -40,7 +152,11 @@ monogatari.assets('music', {});
 monogatari.assets('voices', {});
 
 // Define the sounds used in the game.
-monogatari.assets('sounds', {});
+monogatari.assets('sounds', {
+  'trueno': 'truenos.mp3',
+  'lluvia': 'lluvia.mp3',
+  'fogata': 'fogata.mp3'
+});
 
 // Define the videos used in the game.
 monogatari.assets('videos', {});
@@ -53,71 +169,117 @@ monogatari.assets('scenes', {});
 
 // Define the Characters
 monogatari.characters({
-  y: {
-    name: 'Yui',
-    color: '#5bcaff',
+  r: {
+    name: 'Rey',
+    color: '#FFE628',
+    directory: 'rey',
+    sprites: {
+      normal: 'rey.png',
+    },
+    expressions: {
+      angry: 'expressions/normal.png',
+      normal: 'expressions/normal.png',
+
+    },
+    default_expression: 'normal'
   },
+  m: {
+    name: 'Molineros',
+    color: '#9FBD6E',
+    directory: 'adoptivos',
+    sprites: {
+      normal: 'adoptivos.png',
+    },
+    expressions: {
+      angry: 'expressions/normal.png',
+      normal: 'expressions/normal.png',
+
+    },
+    default_expression: 'normal'
+  },
+
+  a: {
+    name: 'Anciana',
+    color: '#9FBD6E',
+    directory: 'anciana',
+    sprites: {
+      normal: 'anciana.png',
+    },
+    expressions: {
+      angry: 'expressions/normal.png',
+      normal: 'expressions/normal.png',
+
+    },
+    default_expression: 'normal'
+  },
+
+  pr: {
+    name: 'Muchacho',
+    color: '#70DBF7',
+    directory: 'protagonista',
+    sprites: {
+      normal: 'protagonista.png',
+    },
+    expressions: {
+      angry: 'expressions/normal.png',
+      normal: 'expressions/normal.png',
+
+    },
+    default_expression: 'normal'
+  },
+
 });
 
 monogatari.script({
   // The game starts here.
   Escena2: [
-    'show scene #000000 with fadeIn',
-    'show notification Welcome',
-    {
-      Input: {
-        Text: 'What is your name?',
-        Validation: function (input) {
-          return input.trim().length > 0;
-        },
-        Save: function (input) {
-          this.storage({
-            player: {
-              name: input,
-            },
-          });
-          return true;
-        },
-        Revert: function () {
-          this.storage({
-            player: {
-              name: '',
-            },
-          });
-        },
-        Warning: 'You must enter a name!',
-      },
-    },
-    'y Hi {{player.name}} Welcome to Monogatari!',
-    {
-      Choice: {
-        Dialog: 'y Have you already read some documentation?',
-        Yes: {
-          Text: 'Yes',
-          Do: 'jump Yes',
-        },
-        No: {
-          Text: 'No',
-          Do: 'jump No',
-        },
-      },
-    },
-  ],
+    'show scene #191A21 with fadeIn',
+    'show particles rain',
 
-  Yes: [
-    'y Thats awesome!',
-    'y Then you are ready to go ahead and create an amazing Game!',
-    'y I can’t wait to see what story you’ll tell!',
-    'end',
-  ],
 
-  No: [
-    'y You can do it now.',
+    'play sound lluvia with loop',
+    'play sound trueno with volume 50',
+    'centered He aquí que un día el Rey, sorprendido por una tempestad, entró a guarecerse en el molino y preguntó a los molineros si aquel guapo muchacho era hijo suyo.',
+    'hide particles rain',
 
-    'show message Help',
+    'show background #343746 with fadeIn',
 
-    'y Go ahead and create an amazing Game!',
-    'y I can’t wait to see what story you’ll tell!',
-    'end',
+    'show image 2molino.png center with fadeIn',
+
+    'm:normal No, es un niño expósito; hace catorce años que lo encontramos en una caja, en la presa del molino.',
+
+    'Comprendió el Rey que no podía ser otro sino aquel niño de la suerte que había arrojado al río, y dijo.',
+
+    'r:normal Buena gente, ¿dejaríais que el chico llevara una carta mía a la Señora Reina? Le daré en pago dos monedas de oro.',
+    'm:normal ¡Como mande el Señor Rey!',
+    'stop sound lluvia with fade 12',
+
+    'El Rey escribió entonces una carta a la Reina, en los siguientes términos:',
+    'r:normal En cuanto se presente el muchacho con esta carta, lo mandarás matar y enterrar, y esta orden debe cumplirse antes de mi regreso.',
+
+    'Púsose el muchacho en camino con la carta, pero se extravió, y al anochecer llegó a un gran bosque.',
+    'Vio una lucecita en la oscuridad y se dirigió allí, resultando ser una casita muy pequeña.',
+
+    'play sound fogata',
+    'Al entrar sólo había una anciana sentada junto al fuego, la cual se asustó al ver al mozo y le dijo:',
+
+    'a:normal ¿De dónde vienes y adónde vas?',
+
+    'pr:normal Vengo del molino y voy a llevar una carta a la Señora Reina. Pero como me extravié, me gustaría pasar aquí la noche.',
+
+    'a:normal ¡Pobre chico!. Has venido a dar en una guarida de bandidos, y si vienen te matarán.',
+
+    'pr:normal Venga quien venga, no tengo miedo. Estoy tan cansado que no puedo dar un paso más',
+    'stop sound fogata with fade 5',
+
+
+    'Y, tendiéndose sobre un banco, se quedó dormido en el acto.',
+    'A poco llegaron los bandidos y preguntaron, enfurecidos, quién era el forastero que allí dormía.',
+
+    'a:normal  ¡Ay!, es un chiquillo inocente que se extravió en el bosque; lo he acogido por compasión. Parece que lleva una carta para la Reina.',
+    '',
+    '',
+    '',
+    '',
   ],
 });
